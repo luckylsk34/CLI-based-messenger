6291340646952eb4662312f1657c2b22d8efd2de
sai krishna <lakkam.saikrishnareddy@gmail.com>
2018-01-03 23:44:27 +0530

    added backspacing and when the client sends the data the lines written byt the user are not erased
    when the peer network sends the

--- my_messenger client.py
+++ my_messenger client.py
@@ -2,6 +2,7 @@ import socket
 from threading import Thread
 from colorama import winterm
 import sys
+from msvcrt import getche, getch
 
 ###########################################################################################################
 
@@ -12,45 +13,77 @@ w = winterm.WinTerm()
 sendclient = socket.socket()
 recvclient = socket.socket()
 
-host = '192.168.1.107' #+ input('host ip : 192.168.1.')
+host = input('host ip : ')
 recvport = 9998
 sendport = 9997
 
 recvclient.connect((host, recvport))
-print('recieving active')
 sendclient.connect((host, sendport))
-print('sending active')
+print('connected to', sendclient.getsockname()[0], 'on', sendclient.getsockname()[1])
 
 ###########################################################################################################
 
 #this part has functions for sending and recieving messages
 
 n = 0
+msg = ''
 
 def sending():
-	global n
-	while True:
-		msg = input('you: ')
-		n = 0
-		sendclient.send(msg.encode('ascii'))
+    global n, msg
+    while True:
+        global n, msg
+        print('you: ', end = '')
+        while True:
+            while True:
+                cha = getch()
+                if cha == chr(13).encode('ascii'):
+                    print('\nyou: ', end = '')
+                    break
+                elif cha == chr(8).encode('ascii'):
+                    position = w.get_position(-11)
+                    w.cursor_adjust(-1, 0)
+                    print(' ', end = '')
+                    sys.stdout.flush()
+                    w.cursor_adjust(-1, 0)
+                    msg = msg[:-1]
+                else:
+                    try:
+                        print(cha.decode('ascii'), end = '')
+                        sys.stdout.flush()
+                        msg+= cha.decode('ascii')
+                    except:
+                        pass
+            if msg:
+                n = 0
+                msg+= ' '
+                sendclient.send(msg.encode('ascii'))
+                msg = ''
 
 def recieving():
-	global n
-	while True:
-		msg1 = recvclient.recv(1024)
-		pos = w.get_position(-11)
-		if n == 0:
-			n = 1
-			w.cursor_adjust(1-pos.X, 0)
-			print('                                                                                                    ', end = '\r')
-			print('other:', msg1.decode('ascii'))
-			print('\nyou: ', end = '')
-		else:
-			w.cursor_adjust(1-pos.X, -1)
-			print('other:', msg1.decode('ascii'))
-			print('                                                                                                    ', end = '\r')
-			print('you: ', end = '')
-			sys.stdout.flush()
+    global n, msg
+    while True:
+        while True:
+            msg1 = recvclient.recv(1024)
+            pos = w.get_position(-11)
+            if n == 0:
+                n = 1
+                w.cursor_adjust(1-pos.X, 0)
+                print('                                                                                                    ', end = '\r')
+                print('\nother:', msg1.decode('ascii'))
+                if msg:
+                    print('\nyou: '+ msg, end = '')
+                else:
+                    print('\nyou: ', end = '')
+            else:
+                w.cursor_adjust(1-pos.X, -1)
+                count = 0
+                print('other:', msg1.decode('ascii'))
+                print('                                                                                                    ', end = '\r')
+                if msg:
+                    print('\nyou: '+ msg, end = '')
+                else:
+                    print('\nyou: ', end = '')
+                sys.stdout.flush()
 
 #this starts the sending and recieving on two different threads
 
--- my_messenger server.py
+++ my_messenger server.py
@@ -8,6 +8,7 @@ import socket
 from threading import Thread
 from colorama import winterm
 import sys
+from msvcrt import getche, getch
 
 ###########################################################################################################
 
@@ -28,43 +29,72 @@ recvsocket.bind((host, recvport))    #socket for recieving information
 #connection to the client on sending socket on 9998 port
 sendsocket.listen(5)
 sendclientsocket, addr = sendsocket.accept()
-print('sending active')
 
 #connection to the client on recieving socket on 9997 port
 recvsocket.listen(5)
 recvclientsocket, addr = recvsocket.accept()
-print('recieving active')
+print('connected to', recvclientsocket.getsockname()[0], 'on', recvclientsocket.getsockname()[1])
 
 ###########################################################################################################
 
 #this part has the required functions for chatting by reiceving and sending the required messages
 
 n = 0
+msg = ''
 
 def sending():
-	global n
-	while True:
-		msg = input('you: ')
-		n = 0
-		sendclientsocket.send(msg.encode('ascii'))
+    global n, msg
+    print('you: ', end = '')
+    while True:
+        while True:
+            cha = getch()
+            if cha == chr(13).encode('ascii'):
+                print('\nyou: ', end = '')
+                break
+            elif cha == chr(8).encode('ascii'):
+                position = w.get_position(-11)
+                w.cursor_adjust(-1, 0)
+                print(' ', end = '')
+                sys.stdout.flush()
+                w.cursor_adjust(-1, 0)
+                msg = msg[:-1]
+            else:
+                try:
+                    print(cha.decode('ascii'), end = '')
+                    sys.stdout.flush()
+                    msg+= cha.decode('ascii')
+                except:
+                    pass
+        if msg:
+            n = 0
+            msg+= ' '
+            sendclientsocket.send(msg.encode('ascii'))
+            msg = ''
 
 def recieving():
-	global n
-	while True:
-		msg1 = recvclientsocket.recv(1024)
-		pos = w.get_position(-11)
-		if n == 0:
-			n = 1
-			w.cursor_adjust(1-pos.X, 0)
-			print('                                                                                                    ', end = '\r')
-			print('other:', msg1.decode('ascii'))
-			print('\nyou: ', end = '')
-		else:
-			w.cursor_adjust(1-pos.X, -1)
-			print('other:', msg1.decode('ascii'))
-			print('                                                                                                    ', end = '\r')
-			print('you: ', end = '')
-			sys.stdout.flush()
+    global n, msg
+    while True:
+        msg1 = recvclientsocket.recv(1024)
+        pos = w.get_position(-11)
+        if n == 0:
+            n = 1
+            w.cursor_adjust(1-pos.X, 0)
+            print('                                                                                                    ', end = '\r')
+            print('\nother:', msg1.decode('ascii'))
+            if msg:
+                print('\nyou: '+ msg, end = '')
+            else:
+                print('\nyou: ', end = '')
+        else:
+            w.cursor_adjust(1-pos.X, -1)
+            count = 0
+            print('other:', msg1.decode('ascii'))
+            print('                                                                                                    ', end = '\r')
+            if msg:
+                print('\nyou: '+ msg, end = '')
+            else:
+                print('\nyou: ', end = '')
+            sys.stdout.flush()
 #this starts the sending and recievig on two different threads
 print(' ')
 Thread(target = sending).start()
